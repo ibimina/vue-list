62c3d917cf69591a68c2be34990cbdd2a41acd7b
Ibimina <ibimina.c.hart@gmail.com>
2022-12-30 18:39:48 +0100

    docs: language list component

---
+++ src/components/LanguageList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="lang_con">
+    <li
+      v-for="language in languages"
+      :key="language"
+      class="lang_list"
+      @click="handleCompleted(language)"
+    >
+      <p>{{ language.path }}</p>
+      <p>{{ language.name }}</p>
+      <p>{{ language.completed }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["languages"],
+  methods: {
+    handleCompleted(language) {
+      language.completed = !language.completed;
+    },
+  },
+};
+</script>
