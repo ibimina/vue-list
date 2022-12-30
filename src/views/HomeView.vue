1f45af9b75837a165c8d48bdbcf119ba2657c47e
Ibimina <ibimina.c.hart@gmail.com>
2022-12-30 18:39:08 +0100

    docs: vue home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      voluptates, quod, quia, voluptate quae voluptatem quibusdam consequuntur
+      quos voluptatum quas quidem. Quisquam, quae. Quisquam voluptates, quod,
+      quia, voluptate quae voluptatem quibusdam consequuntur quos voluptatum
+      quas quidem. Quisquam, quae.
+    </p>
+    <p>{{ message }}</p>
+    <ul>
+      <li v-for="name in names" :key="name">
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello from HomeView",
+      names: ["John", "Jane", "Joe", "Janet", "Job", "Johndiddles", "Jonathan"],
+    };
+  },
+};
+</script>
